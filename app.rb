9456aa964c0a2d2bb21fbcdfeff510926f93669d
Kirtiman Singh <kirt93@hotmail.com>
2018-07-26 10:57:47 +0100

    sinatra using html forms and params

--- app.rb
+++ app.rb
@@ -4,9 +4,23 @@ set :session_secret, 'super secret'
 get '/' do
   "Hello World"
 end
+
 get '/secret' do
 	"This should be a secret"
 end
-get '/cat' do
+
+get '/random_cat' do
+  @name = %w(Amigo, Oscar, Viking).sample
+  erb(:index)
+end
+
+get '/form' do
+  erb(:forms)
+end
+
+post '/named_cat' do
+  p params
+  @name = params[:name]
+  @food = params[:food]
   erb(:index)
 end
